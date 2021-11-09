cae22afe06f5d4c601e3de840cb507f5ff68259b
Khazaina <>
2021-11-09 22:07:47 +0500

    second commit

--- app/src/main/java/com/example/quiz_kh/MainActivity.java
+++ app/src/main/java/com/example/quiz_kh/MainActivity.java
@@ -2,13 +2,49 @@ package com.example.quiz_kh;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main); Button button = findViewById(R.id.repo);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                String url="https://github.com/Khazaina/quiz_kh";
+                Uri repository = Uri.parse(url);
+                Intent intent = new
+                        Intent(Intent.ACTION_VIEW, repository);
+                startActivity(intent);
+            }
+        });
+        Button learn =findViewById(R.id.learning);
+        learn.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, learning.class);
+                startActivity(intent);
+            }
+        });
+        Button quiz =findViewById(R.id.test);
+        quiz.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, test.class);
+                startActivity(intent);
+            }
+        });
+
+
+
+
+
+
+
+
     }
+
 }
